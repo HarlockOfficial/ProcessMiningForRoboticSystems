a2a1200d3e1cac743ac0b7479cfa59b9306b1730
HarlockOfficial <harlockofficial.github@gmail.com>
2022-12-15 11:09:09 +0100

    [delete] unused files

--- view_process_tree.py
+++
@@ -1,2 +0,0 @@
-import pm4py
-pm4py.visualization.process_tree.variants.wo_decoration.operators_mapping = {"->": "seq", "X": "xor", "+": "and", "*": "xor loop", "O": "or", "<>": "interleaving", "receive_message": "receive_message", "send_message": "send_message"}
